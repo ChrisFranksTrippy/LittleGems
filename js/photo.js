14b2bb663638927f9a81384cf78131a5b9d3e137
Chris Franks <1000frankschris@gmail.com>
2016-11-11 17:11:22 +0000

    Styled and funcitonal photo slider

--- js/photo.js
+++ js/photo.js
@@ -2,25 +2,36 @@ $(function(){
 
   localStorage.photoCount= 1;
 
-  $('.prev').bind('click', function(){
+
+
+  $('.prev div').bind('click', function(){
     var photoCount = Number(localStorage.photoCount);
     localStorage.photoCount = photoCount - 1;
     changeImg();
   });
 
-  $('.next').bind('click', function(){
+  $('.next div').bind('click', function(){
     var photoCount = Number(localStorage.photoCount);
     localStorage.photoCount = photoCount + 1;
     changeImg();
   });
+
+  setInterval(function(){
+    var photoCount = Number(localStorage.photoCount);
+    localStorage.photoCount = photoCount + 1;
+    changeImg();
+  }, 10000);
+
 })
 
 
 function changeImg(){
+  var img = $('.photo-container img');
   var photoCount = Number(localStorage.photoCount);
   if (photoCount < 1)
     localStorage.photoCount = 5;
   else if (photoCount > 5)
     localStorage.photoCount = 1;
-  $('.photo-container img').attr('src','/img/picture' + localStorage.photoCount + '.gif');
+  
+  $(img).attr('src','/img/picture' + localStorage.photoCount + '.gif');
 }
